fce9cf5bd570bdb44f137182007b7557f271745a
Megavannan M D <megavannanmd7@gmail.com>
2025-08-06 16:21:30 +0530

    Console logging payload

--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -5,13 +5,8 @@ export class GithubService {
   private readonly logger = new Logger(GithubService.name);
 
   processEvent(eventType: string, payload: Record<string, unknown>): void {
-    this.logger.log(`🐙 GitHub Event Received: ${eventType}`);
-    this.logger.debug('📦 Payload:', payload);
-
-    // Example: Handle push events
-    if (eventType === 'push') {
-      this.logger.log(`🟢 Push by: ${(payload?.pusher as any)?.name}`);
-      this.logger.log(`📁 Repo: ${(payload?.repository as any)?.full_name}`);
-    }
+    this.logger.log(`GitHub Event Received: ${eventType}`);
+    this.logger.log('Payload:')
+    console.log(payload)
   }
 }
